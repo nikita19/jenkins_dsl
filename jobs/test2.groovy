5da26c537e2dcae9cce8a132ac3aba4bd0e610de
Nykyta_Udut <bandroix@gmail.com>
2017-06-26 13:07:52 +0300

    changed color and testing properties

--- jobs/test2.groovy
+++ jobs/test2.groovy
@@ -1,17 +1,25 @@
 pipelineJob('Pipejob') {
   displayName('Pipeline job')
   description('Creates pipeline job using Jenkins DSL.')
+  properties {
+    githubProjectUrl('https://github.com/nikita19/jenkins_dsl.git')
+  }
+  triggers {
+    githubPush()
+  }
   definition {
   	cps {
   		sandbox()
   		script("""
           node {
           	  try {
-                stage ('1-st stage'){
-                    build "DSL-Tutorial-1-Test"
-                }
-                stage ('2-nd stage'){
-                	echo "build was finished"
+          	  	ansiColor('xterm') {
+                  stage ('\u001B[31m1-st stage'){
+                      build "DSL-Tutorial-1-Test"
+                  }
+                  stage ('\u001B[0m2-nd stage'){
+                  	echo "build was finished"
+                  }
                 }
               } catch (e) {
               	currentBuild.result = 'FAILED'
