6b6f89484123300bfcccbc46aaff208924d90a6e
Nykyta_Udut <bandroix@gmail.com>
2017-07-27 13:07:13 +0300

    nagios fixed default , and cred parameter added

--- jobs/nagios.groovy
+++ jobs/nagios.groovy
@@ -1,11 +1,12 @@
 pipelineJob('Nagios_job') {
   displayName('Set nagios downtime')
   description('Set nagios downtime using Jenkins DSL.')
+  passwordParameterDefinition {name('Credentials to nagios server'),  defaultValue(nagios), description('Nagios credentials')}
   parameters {
-            stringParam('HOST', "", 'HOST description')
-            stringParam('MINUTES', "", 'MINUTES description')
-            stringParam('COMMENT', "", 'COMMENT description')
-            stringParam('SERVICES', "", 'SERVICES description')
+            stringParam('HOST', 'localhost', 'HOST description')
+            stringParam('SERVICES', 'PING,HTTP', 'SERVICES description')
+            stringParam('MINUTES', '30', 'MINUTES description')
+            stringParam('COMMENT', 'Run by jenkins', 'COMMENT description')
   }
   definition {
   	cps {
