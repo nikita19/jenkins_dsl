a5d870e182d4958b9039ca5021bb49ba6f922a11
Nykyta_Udut <bandroix@gmail.com>
2017-07-31 13:25:28 +0300

    added improved delete dsl job

---
+++ nagios/nagios_delete_downtime.groovy
@@ -0,0 +1,29 @@
+String directory = 'examples'
+
+folder(directory) {
+    description 'Contains jobs for graphite stack'
+}
+
+pipelineJob("delete downtimes(dsl)") {
+    parameters {
+    credentialsParam('Credentials to nagios server') {
+        type('com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl')
+        required()
+        defaultValue('nagios')
+        description('SSH Key for deploying build artifacts')
+        }
+    stringParam('HOST', 'localhost', 'HOST description')
+    stringParam('SERVICES', 'PING,HTTP', 'SERVICES description')
+    }
+    definition {
+        logRotator {
+            numToKeep(5)
+            artifactNumToKeep(1)
+            daysToKeep(7)
+        }
+        cps {
+            sandbox()
+            script(readFileFromWorkspace('scripts/nagios_delete_downtime_script.groovy'))
+        }   // cps
+    }       // definition
+}           // job
---
+++ scripts/nagios_delete_downtime_script.groovy
@@ -0,0 +1,36 @@
+node {
+    NAGIP="10.128.46.200"
+    // USER="nagiosadmin"
+    // PASS="1q2w3e"
+    def splitService = SERVICES.split(",")
+    for (int i = 0; i < splitService.length; i++) {
+        stage ("Set downtime for nagios host: ${HOST} service: ${splitService[i]}"){
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'nagios',
+                            usernameVariable: 'USER', passwordVariable: 'PASS']]) {
+            if (env.SERVICES == '') {
+                cmd_type = "78"
+                dataparam = "query=downtimelist&hostname=${HOST}&downtimeobjecttypes=host"
+                println "Deleting downtime on hosts"
+                println dataparam
+            }
+            else {
+                cmd_type = "79"
+                dataparam = "query=downtimelist&hostname=${HOST}&servicedescription=${splitService[i]}&downtimeobjecttypes=service"
+                println "Deleting downtime on services"
+                println dataparam
+            }
+                sh """#!/usr/bin/env bash
+downtimelist=\$(curl --silent -k -u "${USER}:${PASS}"  https://${NAGIP}/nagios/cgi-bin/statusjson.cgi -d "${dataparam}" | python -c 'import json,sys;data=json.load(sys.stdin);print data["data"]["downtimelist"]')
+echo "\$downtimelist"
+filteredList=`echo \$downtimelist | tr -d '[,]'`
+for id in \$filteredList; do curl --silent --show-error  --data cmd_typ=${cmd_type} --data cmd_mod=2 --data down_id=\$id  --data "com_data=Updating+application" --data btnSubmit=Commit --insecure https://10.128.46.200/nagios/cgi-bin/cmd.cgi -u "nagiosadmin:1q2w3e"; done
+echo "DONE"
+                """   
+
+            } //creds
+            
+        } // stage
+    }     // for
+}         // node
+
+
