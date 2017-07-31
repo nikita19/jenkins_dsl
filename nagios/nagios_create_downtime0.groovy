7c2f2e0435db8e03836dcdfdfcab0d99c49b6f8c
Nykyta_Udut <bandroix@gmail.com>
2017-07-31 12:42:11 +0300

    added new nagios create job

---
+++ nagios/nagios_create_downtime0.groovy
@@ -0,0 +1,19 @@
+String directory = 'examples'
+
+folder(directory) {
+    description 'Contains jobs for graphite stack'
+}
+
+pipelineJob("create downtimes(dsl)") {
+    definition {
+        logRotator {
+            numToKeep(5)
+            artifactNumToKeep(1)
+            daysToKeep(7)
+        }
+        cps {
+            sandbox()
+            script(readFileFromWorkspace('jenkins-jobs/nagios_create_downtime.groovy'))
+        }
+    }
+}
